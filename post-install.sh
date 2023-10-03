14f75106283e3a8ab57caf1781259403d6d1f760
Horacio Daniel Ros <horaciodrs@gmail.com>
2023-10-02 22:18:54 -0300

    post install copia los archivos necesarios

--- post-install.sh
+++ post-install.sh
@@ -20,3 +20,24 @@ EOF
 # Configuración de permisos para el archivo de inicio de sesión.
 chown $username:$username /home/$username/.config/autostart/plank.desktop
 chmod +x /home/$username/.config/autostart/plank.desktop
+
+# Copia el tema de GTK
+cp -r /root/leopard-post-install/Leopard-linuc-theme /usr/share/themes
+
+# Copia el tema de iconos
+cp -r /root/leopard-post-install/Leopard-linux-icons /usr/share/icons
+
+# Copia las fuentes
+cp -r /root/leopard-post-install/fonts/FiraCode /usr/share/fonts/truetype
+cp -r /root/leopard-post-install/fonts/SanFrancisco /usr/share/fonts/truetype
+
+# Coopia la configuracion local del usuario
+mkdir -p /home/$username/.config
+cp -r /root/leopard-post-install/config/* /home/$username/.config/
+
+# Copia los archivos locales al directorio del usuario
+mkdir -p /home/$username/.local
+cp -r /root/leopard-post-install/local/* /home/$username/.local
+
+# Copia los fondos de pantalla
+cp -r /root/leopard-post-install/wallpapers/* /usr/share/images/desktop-base
